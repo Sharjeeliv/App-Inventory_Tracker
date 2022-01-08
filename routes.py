c0b5bdfee2dbdcced5e7fd7269798a751a01799a
Sharjeel Mustafa <sharjeeliv@gmail.com>
2022-01-08 16:51:19 -0500

    Created frontend with dummy data

--- routes.py
+++ routes.py
@@ -2,11 +2,24 @@ from flask import Blueprint, render_template
 
 routes = Blueprint("routes", __name__)
 
+inventory = [
+    {'name': 'Iphone',
+     'manufacturer': 'Apple',
+     'quantity': '100',
+     'summary': 'This is a high tech device'
+     },
+    {'name': 'Pizza',
+     'manufacturer': 'PizzaHut',
+     'quantity': '50',
+     'summary': 'This is very yummy'
+     }
+]
+
 
 @routes.route('/')
 @routes.route('/home')
 def home():
-    return render_template('main.html')
+    return render_template('main.html', inventory=inventory)
 
 
 @routes.route('/new-item')
