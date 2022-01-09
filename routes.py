2d983a0d95f4e54fbb103670a2d7e10c576dba46
Sharjeel Mustafa <sharjeeliv@gmail.com>
2022-01-09 15:49:03 -0500

    Completed new item page, started work on integrating database

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from forms import NewItemForm, UpdateItemForm
 
 routes = Blueprint("routes", __name__)
 
@@ -19,9 +20,19 @@ inventory = [
 @routes.route('/')
 @routes.route('/home')
 def home():
-    return render_template('main.html', inventory=inventory)
+    return render_template('homepage.html', inventory=inventory)
 
 
-@routes.route('/new-item')
-def item():
-    return render_template('sidebar.html')
+@routes.route('/new-item', methods=['GET', 'POST'])
+def new_item():
+    form = NewItemForm()
+    if form.validate_on_submit():
+        flash(f'Item entry for {form.name.data} created!', 'success')
+        return redirect(url_for('routes.home'))
+    return render_template('newitem.html', form=form)
+
+
+@routes.route('/update-item')
+def update_item():
+    form = UpdateItemForm()
+    return render_template('update-item.html', form=form)
